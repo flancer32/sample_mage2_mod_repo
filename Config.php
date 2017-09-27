9aa7f1b482053f9162bc557e01fe6d962b27a74b
Alex Gusev <alex@flancer64.com>
2017-09-27 10:30:06 +0300

    Init

---
+++ Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Hardcoded configuration for the module.
+ *
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\SampleRepo;
+
+class Config
+{
+
+    /**
+     * This module's name.
+     */
+    const MODULE = 'Flancer32_SampleRepo';
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sample_mage2_mod_repo
-Sample of Magento 2 module to be deployed from GitHub
+
+Simple Magento 2 module to be deployed directly from GitHub.
+
+This module is used in https://github.com/flancer32/sample_mage2_app
